3a0cf36330352dc7e80493629c9c8503582a754c
nsornman <164116976+nsornman@users.noreply.github.com>
2024-06-14 21:14:49 +0700

    fghfgfg
    fgh

---
+++ cardgame/Assets/Modle/NewBehaviourScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Animator animator;
+    int isWalkHash;
+    int isRunHash;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isWalkHash = Animator.StringToHash("isWalk");
+        isRunHash = Animator.StringToHash("isRun");
+    }
+
+    void Update()
+    {
+        bool isRun = animator.GetBool(isRunHash);
+        bool isWalk = animator.GetBool(isWalkHash);
+        bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+
+        if (!isWalk && forwardPressed)
+        {
+            animator.SetBool(isWalkHash, true);
+        }
+
+        if (isWalk && !forwardPressed)
+        {
+            animator.SetBool(isWalkHash, false);
+        }
+
+        if (!isRun && (isWalk && runPressed))
+        {
+            animator.SetBool(isRunHash, true);
+        }
+
+        if (isRun && (!isWalk || !runPressed))
+        {
+            animator.SetBool(isRunHash, false);
+        }
+
+    }
+}
