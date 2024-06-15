36664acbaee0dde7875e26936854b9eaaba472f2
TBetter00 <125780472+TBetter00@users.noreply.github.com>
2024-06-15 15:59:51 +0700

    add inventory system

---
+++ cardgame/Assets/Script/InventorySystem/InventoryManager.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    public Skillslot[] skillslots;
+    public Contructslot[] constructslots;
+    public CardItem[] card;
+    public SkillCard[] SC;
+    public ConstructionCard[] CC;
+    void Start()
+    {
+        skillslots = FindObjectsOfType<Skillslot>();
+        constructslots = FindObjectsOfType<Contructslot>();
+    }
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A)){
+            AddSkillitem(SC[randomSCindice()]);
+        }
+        if(Input.GetKeyDown(KeyCode.D)){
+            AddConstructitem(CC[randomCCindice()]);
+        }
+        if(Input.GetKeyDown(KeyCode.Tab)){
+            if(Checkfullslot()){
+                Debug.Log("All slot is full");
+            }else{
+                Debug.Log("Some slot is not full");
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.E)){
+            EmptyAllSlot();
+        }
+    }
+    public int randomSCindice(){
+        if (SC.Length == 0)
+        {
+            Debug.Log("No cards available");
+        }
+        return Random.Range(0, SC.Length);
+    } 
+    public int randomCCindice(){
+        if (CC.Length == 0)
+        {
+            Debug.Log("No cards available");
+        }
+        return Random.Range(0, CC.Length);
+    } 
+
+    public void AddSkillitem(CardItem card){
+        string cardname = card.CardName;
+        Sprite cardsprite = card.Cardsprite;
+        for (int i = 0; i < skillslots.Length; i++)
+        {
+            if(!skillslots[i].isfull){
+                skillslots[i].Additem(cardname , cardsprite, card);
+                return;
+            }
+        }
+    }
+    public void AddConstructitem(CardItem card){
+        string cardname = card.CardName;
+        Sprite cardsprite = card.Cardsprite;
+        for (int i = 0; i < constructslots.Length; i++)
+        {
+            if(!constructslots[i].isfull){
+                constructslots[i].Additem(cardname , cardsprite, card);
+                return;
+            }
+        }
+    }
+    public bool Checkfullslot(){
+        bool slotfull = true;
+        for(int i= 0;i< skillslots.Length;i++){
+            if(!skillslots[i].isfull){
+                slotfull = false;
+            }
+        }
+        for(int i= 0;i< constructslots.Length;i++){
+            if(!constructslots[i].isfull){
+                slotfull = false;
+            }
+        }
+        if(slotfull){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public void DeselectedAllSlot(){
+        for (int i = 0; i < skillslots.Length; i++)
+        {
+            skillslots[i].SelectedPanel.SetActive(false);
+            skillslots[i].selected = false;
+        }
+        for (int i = 0; i < constructslots.Length; i++)
+        {
+            constructslots[i].SelectedPanel.SetActive(false);
+            constructslots[i].selected = false;
+        }
+    }
+    void EmptyAllSlot(){
+        for (int i = 0; i < skillslots.Length; i++)
+        {
+            skillslots[i].Useitem();
+        }
+        for (int i = 0; i < constructslots.Length; i++)
+        {
+            constructslots[i].Useitem();
+        }
+    }
+
+}
---
+++ cardgame/Assets/Script/InventorySystem/Skillslot.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Skillslot : MonoBehaviour , IPointerClickHandler
+{
+    //=======Item Data=======//
+    private string itemname;
+    private Sprite itemimage;
+    [HideInInspector]public bool isfull = false;
+    public CardItem card;
+   
+    //=====Item Slot=====// 
+    [Header("Item Slot")]
+    [SerializeField] private TMP_Text cardname;
+    [SerializeField] private Image cardimage;
+    
+    
+    public GameObject SelectedPanel;
+    public bool selected = false;
+
+    private InventoryManager IM;
+
+    void Start()
+    {
+        IM = FindObjectOfType<InventoryManager>();
+    }
+    public void Additem(string name, Sprite cardsprite, CardItem card){
+        this.itemname = name;
+        this.itemimage = cardsprite;
+        isfull = true;
+        this.card = card;
+
+        cardname.text = itemname.ToString();
+        if(!cardimage.gameObject.activeSelf){
+            cardimage.gameObject.SetActive(true);
+        }
+        this.cardimage.sprite = itemimage;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button == PointerEventData.InputButton.Left){
+            OnLeftClick();
+        }
+        if(eventData.button == PointerEventData.InputButton.Right){
+            OnRightClick();
+        }
+    }
+
+    void OnLeftClick(){
+        if (selected)
+        {
+            // Deselect this slot
+            selected = false;
+            SelectedPanel.SetActive(false);
+        }
+        else
+        {
+            // Deselect all other slots
+            IM.DeselectedAllSlot();
+            // Select this slot
+            selected = true;
+            SelectedPanel.SetActive(true);
+        }
+    }
+
+    void OnRightClick(){
+        if(selected){
+            Useitem();
+            selected = false;
+            SelectedPanel.SetActive(false);
+        }
+    }
+
+    public void Useitem(){
+        if(isfull){
+            isfull = false;
+            Emptyslot();
+        }
+        
+    }
+    void Emptyslot(){
+        this.cardname.text = "Emptycard";
+        this.cardimage.gameObject.SetActive(false);
+    }
+}
