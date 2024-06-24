dda3c97773e1c6a8e6b0165e5f4ca2a4a6563f34
kungnawanghin <kungnawanghin@gmail.com>
2024-06-24 21:33:05 +0700

    change
    code ui

--- cardgame/Assets/Script/PlayButtonScript.cs
+++ cardgame/Assets/Script/PlayButtonScript.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayButtonScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadSceneAsync(1);
     }
 }
